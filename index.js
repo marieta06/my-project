849380d8e06a130aee5f7392618dc56220a77ba6
marieta06 <marieta.sahakyan.8@gmail.com>
2022-05-08 07:05:29 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const HOSTNAME = '127.0.0.1';
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.end('Hello World');
+});
+
+server.listen(PORT, HOSTNAME, () => {
+    console.log(`Server running at http://${HOSTNAME}:${PORT}`);
+});
