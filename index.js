56d7734eafe090744e67440c8dd8664331e35c6a
marieta06 <marieta.sahakyan.8@gmail.com>
2022-05-09 02:58:50 -0700

    adding app.js and separate create server logic

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-const http = require('http');
+const server = require('./src/app');
 
 const HOSTNAME = '127.0.0.1';
 const PORT = 3000;
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.end('Hello World');
-});
 
 server.listen(PORT, HOSTNAME, () => {
     console.log(`Server running at http://${HOSTNAME}:${PORT}`);
