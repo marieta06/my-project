82a3c429c3bf8746d29ce3f1f20d83983a7efa2d
Marieta <marieta.sahakyan.8@gmail.com>
2022-05-15 06:21:15 -0700

    read from file, to sort and write in file

--- homework/file-system/index.js
+++ homework/file-system/index.js
@@ -1,4 +1,4 @@
-const util = require("/Users/Marieta/WebstormProjects/my-project/homework/file-system/util.js");
+const util = require("./util.js");
 let string = util.readFile('./homework/file-system/file.txt', 'utf-8');
 let data = util.sortArr(string);
 util.writeFile('./homework/file-system/file.txt', data, 'utf-8');
